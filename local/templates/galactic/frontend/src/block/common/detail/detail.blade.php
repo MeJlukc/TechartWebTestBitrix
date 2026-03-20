4fc22a06496f8337c5330a550cc0ff57416006d4
Yaroslav <solomatin.yarik@mail.ru>
2026-03-20 12:29:00 +0300

    added detail block

---
+++ local/templates/galactic/frontend/src/block/common/detail/detail.blade.php
@@ -0,0 +1,27 @@
+<div class="{{ $block }}">
+    <div class="{{ $block->elem('info-container') }}">
+        <span class="{{ $block->elem('date') }}">{{ $date }}</span>
+        <h2 class="{{ $block->elem('title') }}">
+            {!! $title !!}
+        </h2>
+        <span class="{{ $block->elem('content') }}">
+            {!! $content !!}
+        </span>
+        {!!
+            $renderer->renderBlock(
+                'common/button', 
+                [
+                'content' => 'Назад к новостям',
+                'path' => $href,
+                ],
+            )
+        !!}
+    </div>
+    <div class="{{ $block->elem('image-container') }}">
+        <img 
+        class="{{ $block->elem('image') }}"
+        src="{!! $image_path !!}" 
+        alt="Detail image"
+        >
+    </div>
+</div>
