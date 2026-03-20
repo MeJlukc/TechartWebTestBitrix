9a1fbdc42faf78b168788bada23716fbb396dc44
Yaroslav <solomatin.yarik@mail.ru>
2026-03-20 14:07:42 +0300

    added tags block

---
+++ local/templates/galactic/frontend/src/block/common/tags/tags.blade.php
@@ -0,0 +1,8 @@
+<div class="{{ $block }}">
+    <span class="{{ $block->elem('title') }}">
+        Категории: 
+    </span>
+    <span class="{{ $block->elem('content') }}">
+        {!! $content !!}
+    </span>
+</div>
