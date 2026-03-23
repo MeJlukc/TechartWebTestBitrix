0d5d193b5422672c4ec6fc3e134aa76ecc9fff90
Yaroslav <solomatin.yarik@mail.ru>
2026-03-23 17:23:12 +0300

    added phone field in form and updated dependencies

--- local/templates/galactic/frontend/src/block/forms/forms-form/forms-form.js
+++ local/templates/galactic/frontend/src/block/forms/forms-form/forms-form.js
@@ -1,6 +1,12 @@
+import Inputmask from "inputmask";
+
 document.addEventListener('DOMContentLoaded', () => {
 	const form = document.querySelector('.b-forms-form');
 	const errorContainer = document.querySelector('.b-forms-form__errors');
+	const phoneField = document.querySelector('[name=form_text_8]');
+
+	let phoneMask = new Inputmask("7-999-999-99-99");
+	phoneMask.mask(phoneField);
 
 	if (!form) {
 		return;
@@ -9,21 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
 	form.addEventListener('submit', (event) => {
 		errorContainer.textContent = '';
 
-		const fields = [
+		const requiredFields = [
 			{ name: 'Имя', value: form.elements['form_text_1'].value.trim() },	
-			{ name: 'Email', value: form.elements['form_email_2'].value.trim() },
 			{ name: 'Сообщение', value: form.elements['form_textarea_3'].value.trim() },
 			{ name: 'Категории', value: form.elements['form_text_6'].value.trim() },
 		];
 
+		const emailValue = form.elements['form_email_2'].value.trim();
 		const checkbox = form.elements['form_checkbox_PROCESSING_POLICY[]'];
 
 		const errors = [];
 
-		for (let { name, value } of fields) {
+		for (let { name, value } of requiredFields) {
 			if (value.length < 3) {
 				errors.push(`<p>Заполните обязательное поле "${name}"</p>`);
 			}
+		}
+
+		if ((emailValue.length < 3) && !phoneField.inputmask.isComplete()) {
+			errors.push('<p>Необходимо заполнить email или номер телефона</p>');
 		}
 
 		if (!checkbox.checked) {
