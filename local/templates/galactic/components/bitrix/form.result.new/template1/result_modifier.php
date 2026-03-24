2fe43314560ed32f04db61940b1a95f021a9a20c
Yaroslav <solomatin.yarik@mail.ru>
2026-03-24 08:31:03 +0300

    updated result_modifier in form

--- local/templates/galactic/components/bitrix/form.result.new/template1/result_modifier.php
+++ local/templates/galactic/components/bitrix/form.result.new/template1/result_modifier.php
@@ -25,7 +25,7 @@ $arResult["CUSTOM_FIELDS"]["NAME"] = \TAO::frontend()->renderBlock(
 	[
 	'label' => "Введите имя",
 	'input_field' => $arResult["QUESTIONS"]["USER_NAME"]["HTML_CODE"],
-	'isRequired' => $arResult["QUESTIONS"]["QUESTION_THEME"]["REQUIRED"] == "Y" ? true : null,
+	'isRequired' => $arResult["QUESTIONS"]["USER_NAME"]["REQUIRED"] == "Y" ? true : null,
 	'arResult' => $arResult,
 	]
 );
@@ -35,7 +35,17 @@ $arResult["CUSTOM_FIELDS"]["EMAIL"] = \TAO::frontend()->renderBlock(
 	[
 	'label' => "Введите email",
 	'input_field' => $arResult["QUESTIONS"]["USER_EMAIL"]["HTML_CODE"],
-	'isRequired' => $arResult["QUESTIONS"]["QUESTION_THEME"]["REQUIRED"] == "Y" ? true : null,
+	'isRequired' => $arResult["QUESTIONS"]["USER_EMAIL"]["REQUIRED"] == "Y" ? true : null,
+	'arResult' => $arResult,
+	]
+);
+
+$arResult["CUSTOM_FIELDS"]["PHONE"] = \TAO::frontend()->renderBlock(
+	'forms/forms-input',
+	[
+	'label' => "Введите номер телефона",
+	'input_field' => $arResult["QUESTIONS"]["PHONE_NUMBER"]["HTML_CODE"],
+	'isRequired' => $arResult["QUESTIONS"]["PHONE_NUMBER"]["REQUIRED"] == "Y" ? true : null,
 	'arResult' => $arResult,
 	]
 );
@@ -45,7 +55,7 @@ $arResult["CUSTOM_FIELDS"]["MESSAGE"] = \TAO::frontend()->renderBlock(
 	[
 	'label' => "Ваше сообщение",
 	'textarea_field' => $arResult["QUESTIONS"]["USER_MESSAGE"]["HTML_CODE"],
-	'isRequired' => $arResult["QUESTIONS"]["QUESTION_THEME"]["REQUIRED"] == "Y" ? true : null,
+	'isRequired' => $arResult["QUESTIONS"]["USER_MESSAGE"]["REQUIRED"] == "Y" ? true : null,
 	'arResult' => $arResult,
 	]
 );
@@ -65,7 +75,7 @@ $arResult["CUSTOM_FIELDS"]["PROCESSING_POLICY"] = \TAO::frontend()->renderBlock(
 	'forms/forms-checkbox',
 	[
 	'checkbox_field' => $arResult["QUESTIONS"]["PROCESSING_POLICY"]["HTML_CODE"],
-	'isRequired' => $arResult["QUESTIONS"]["QUESTION_THEME"]["REQUIRED"] == "Y" ? true : null,
+	'isRequired' => $arResult["QUESTIONS"]["PROCESSING_POLICY"]["REQUIRED"] == "Y" ? true : null,
 	'arResult' => $arResult,
 	]
 );
