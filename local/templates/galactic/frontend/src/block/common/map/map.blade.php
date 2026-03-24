3fd219279064f70079a2752292cf8d2c2ccc3653
Yaroslav <solomatin.yarik@mail.ru>
2026-03-24 17:29:38 +0300

    updated map

--- local/templates/galactic/frontend/src/block/common/map/map.blade.php
+++ local/templates/galactic/frontend/src/block/common/map/map.blade.php
@@ -1,15 +1,31 @@
 <div class="{{ $block }}">
 
-    <div class="{{ $block->elem('switcher') }}" data-content="{{ json_encode($content) }}">
-        <button class="{{ $block->elem('switcher-button') }}" data-city="tula">Тула</button>
-        <button class="{{ $block->elem('switcher-button') }}" data-city="moscow">Москва</button>
+    <div class="{{ $block->elem('switcher') }}">
+        @foreach ($content as $key => $value)
+        <button 
+            class="{{ $block->elem('switcher-button')->mod(['active' => $key == 'tula']) }}" 
+            data-key="{{ $key }}"
+            data-coordinates="{{ json_encode($value['coordinates']) }}"
+            data-title="{{ $value['title'] }}"
+            data-description="{{ json_encode($value['description']) }}"
+        >
+            {{ $value['title'] }}
+        </button>
+        @endforeach
     </div>
 
     <div id="map" class="{{ $block->elem('container') }}"></div>
 
     <div class="{{ $block->elem('info') }}">
-        <h3 class="{{ $block->elem('title') }}"></h3>
-        <span class="{{ $block->elem('description') }}"></span>
+        @foreach ($content as $key => $value)
+        <div 
+            class="{{ $block->elem('item')->mod(['active' => $key == 'tula']) }}" 
+            data-key="{{ $key }}"
+        >
+            <h3 class="{{ $block->elem('title') }}">{{ $value['title']}}</h3>
+            <span class="{{ $block->elem('description') }}">{!! join('<br>', $value['description']) !!}</span>
+        </div>
+        @endforeach
     </div>
 
 </div>
--- local/templates/galactic/frontend/src/block/common/map/map.js
+++ local/templates/galactic/frontend/src/block/common/map/map.js
@@ -4,12 +4,15 @@ async function initMap() {
 	const mapContainer = document.getElementById('map');
 	if (!mapContainer) return;
 
-	const switcherBlock = document.querySelector('.b-map__switcher');
+	// Получаем элементы
+
 	const switcherButtons = document.querySelectorAll('.b-map__switcher-button');
 	const activeButtonClass = "b-map__switcher-button--active";
-	const content = JSON.parse(switcherBlock.dataset.content);
-	const infoBlockTitle = document.querySelector('.b-map__title');
-	const infoBlockDescription = document.querySelector('.b-map__description');
+
+	const infoWindows = document.querySelectorAll('.b-map__item');
+	const activeWindowClass = "b-map__item--active";
+
+	// Создаем карту
 
 	const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = window.ymaps3;
 	const {YMapDefaultMarker} = await window.ymaps3.import('@yandex/ymaps3-markers@0.0.1');
@@ -17,20 +20,21 @@ async function initMap() {
 	const map = new YMap(
 		mapContainer,
 		{
-			location: { center: content.tula.coordinates, zoom: 15 }
+			location: { center: [37.584685, 54.200802], zoom: 15 }
 		}
 	);
 
 	map.addChild(new YMapDefaultSchemeLayer());
-
 	map.addChild(new YMapDefaultFeaturesLayer());
 
+	// Ставим базовый маркер по умолчанию
+
 	const marker = new YMapDefaultMarker({
-		coordinates: content.tula.coordinates,
-		title: `<h3>${content.tula.title}</h3>`,
+		coordinates: [37.584685, 54.200802],
+		title: '<h3>Офис в Туле</h3>',
 		color: 'red',
 		popup: {
-			content: `<h3>${content.tula.title}</h3>${content.tula.description.join('<br>')}`,
+			content: `300041, г. Тула, ул. Ф. Смирнова ул., д. 28, оф. 601-602, 701, 703-707, 712 Тел. / Факс: (4872) 250-450, 57-05-01`,
 			position: 'right',
 			hideOnClose: true
 		}
@@ -38,16 +42,15 @@ async function initMap() {
 
 	map.addChild(marker);
 
-	infoBlockTitle.innerHTML = `<h3>${content.tula.title}</h3>`;
-	infoBlockDescription.innerHTML = content.tula.description.join('<br>');
+	// Функция-обработчик при нажатии на ссылку страницы
 
-	switcherButtons[0].classList.add(activeButtonClass);
+	function changeWindow(clickedButton) {
+		const key = clickedButton.dataset.key;
+		const title = clickedButton.dataset.title;
+		const coordinates = JSON.parse(clickedButton.dataset.coordinates);
+		const description = JSON.parse(clickedButton.dataset.description);
 
-	function changeCity(city, clickedButton) {
-		const { coordinates, title, description} = content[city];
-
-		infoBlockTitle.innerHTML = `<h3>${title}</h3>`;
-		infoBlockDescription.innerHTML = description.join('<br>');
+		// Обновлняем карту и метку
 
 		map.update({
 			location: { center: coordinates, zoom: 15 }
@@ -57,19 +60,31 @@ async function initMap() {
 			coordinates: coordinates,
 			title: `<h3>${title}</h3>`,
 			popup: {
-				content: `<h3>${title}</h3>${description.join('<br>')}`,
+				content: description.join('\n'),
 			}
 		});
 
 		map.addChild(marker);
 
+		// Удаляем активные классы с предыдущих элементов и даем новым
+
 		switcherButtons.forEach((button) => button.classList.remove(activeButtonClass));
 		clickedButton.classList.add(activeButtonClass);
+
+		infoWindows.forEach((item) => {
+			item.classList.remove(activeWindowClass);
+			
+			if (item.dataset.key === key) {
+				item.classList.add(activeWindowClass);
+			}
+		});
 	}
+
+	// Навешиваем обработчик
 
 	switcherButtons.forEach((button) => {
 		button.addEventListener('click', () => {
-			changeCity(button.dataset.city, button)
+			changeWindow(button)
 		})
 	})
 }
