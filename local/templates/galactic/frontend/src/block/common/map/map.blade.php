1410ff5bcb5dc69937713db2b7c4012a2c6418da
Yaroslav <solomatin.yarik@mail.ru>
2026-03-24 14:05:30 +0300

    updated map (added variables points and descriptions)

--- local/templates/galactic/frontend/src/block/common/map/map.blade.php
+++ local/templates/galactic/frontend/src/block/common/map/map.blade.php
@@ -1,8 +1,19 @@
 <div class="{{ $block }}">
+
+    <div class="{{ $block->elem('switcher') }}" data-content="{{ json_encode($content) }}">
+        <button class="{{ $block->elem('switcher-button') }}" data-city="tula">Тула</button>
+        <button class="{{ $block->elem('switcher-button') }}" data-city="moscow">Москва</button>
+    </div>
+
     <div id="map" class="{{ $block->elem('container') }}"></div>
+
     <div class="{{ $block->elem('info') }}">
-        @foreach ($content as $item)
-        <span class="{{ $block->elem('item') }}">{!! $item !!}</span>
+        <h3 class="{{ $block->elem('title') }}">
+            {{ $content['tula']['title'] }}
+        </h3>
+        @foreach ($content['tula']['description'] as $item)
+            <span class="{{ $block->elem('item') }}">{!! $item !!}</span>
         @endforeach
     </div>
+
 </div>
--- local/templates/galactic/frontend/src/block/common/map/map.js
+++ local/templates/galactic/frontend/src/block/common/map/map.js
@@ -1,23 +1,21 @@
 async function initMap() {
-
 	await window.ymaps3.ready;
 
 	const mapContainer = document.getElementById('map');
 	if (!mapContainer) return;
 
-	const markerContent = document.querySelector('.b-map__info');
-	const markerValue = markerContent ? markerContent.innerHTML : null;
+	const switcherBlock = document.querySelector('.b-map__switcher');
+	const switcherButtons = document.querySelectorAll('.b-map__switcher-button');
+	const content = JSON.parse(switcherBlock.dataset.content);
+	const infoBlock = document.querySelector('.b-map__info');
 
 	const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = window.ymaps3;
 	const {YMapDefaultMarker} = await window.ymaps3.import('@yandex/ymaps3-markers@0.0.1');
 
 	const map = new YMap(
-		document.getElementById('map'),
+		mapContainer,
 		{
-			location: {
-				center: [37.584685, 54.200802],
-				zoom: 15
-			}
+			location: { center: content.tula.coordinates, zoom: 15 }
 		}
 	);
 
@@ -26,19 +24,46 @@ async function initMap() {
 	map.addChild(new YMapDefaultFeaturesLayer());
 
 	const marker = new YMapDefaultMarker({
-		coordinates: [37.584685, 54.200802],
-		title: 'Techart',
-		subtitle: '',
+		coordinates: content.tula.coordinates,
+		title: `<h3>${content.tula.title}</h3>`,
 		color: 'red',
-
 		popup: {
-			content: markerValue,
+			content: `<h3>${content.tula.title}</h3>${content.tula.description.join('<br>')}`,
 			position: 'right',
 			hideOnClose: true
 		}
 	});
 
 	map.addChild(marker);
+
+	infoBlock.innerHTML = `<h3>${content.tula.title}</h3>${content.tula.description.join('<br>')}`;
+
+
+	function changeCity(city) {
+		const { coordinates, title, description} = content[city];
+
+		infoBlock.innerHTML = `<h3>${title}</h3>${description.join('<br>')})}`;
+
+		map.update({
+			location: { center: coordinates, zoom: 15 }
+		});
+
+		marker.update({
+			coordinates: coordinates,
+			title: `<h3>${title}</h3>`,
+			popup: {
+				content: `<h3>${title}</h3>${description.join('<br>')}`,
+			}
+		});
+
+		map.addChild(marker);
+	}
+
+	switcherButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			changeCity(button.dataset.city)
+		})
+	})
 }
 
 initMap();
