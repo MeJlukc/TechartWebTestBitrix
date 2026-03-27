01eb9395eb6262b8f7b102ccdf301f12ac03b0bd
Yaroslav <solomatin.yarik@mail.ru>
2026-03-27 14:29:33 +0300

    updated books page (migration to frontend)

--- local/templates/galactic/components/bitrix/catalog.item/.default/card/template.php
+++ local/templates/galactic/components/bitrix/catalog.item/.default/card/template.php
@@ -29,7 +29,7 @@ use Bitrix\Main\Localization\Loc;
  * @var CatalogSectionComponent $component
  */
 
-
+/*
 ?>
 <div class="product-item">
 
@@ -42,46 +42,46 @@ use Bitrix\Main\Localization\Loc;
 		<span class="product-item-image-wrapper" data-entity="image-wrapper">
 	<? endif; ?>
 
-		<span class="product-item-image-slider-slide-container slide" id="<?=$itemIds['PICT_SLIDER']?>">
-			<!-- для слайдера, но если уберу - НЕ БУДЕТ АНИМАЦИИ ПРИ НАВЕДЕНИИ (контент убрал) -->
-		</span>
-		<span class="product-item-image-original" id="<?=$itemIds['PICT']?>"
-			style="background-image: url('<?=$item['PREVIEW_PICTURE']['SRC']?>'); <?=($showSlider ? 'display: none;' : '')?>">
-		</span>
-		
-		<?
-		// БЕЗ ЭТОГО НЕ ОТОБРАЖАЕТСЯ ИНФОРМАЦИЯ ПРИ НАВЕДЕНИИ (ДОП ПОЛЯ КОЛИЧЕСТВА И КНОПКИ)
-		if ($item['SECOND_PICT'])
-		{
-			$bgImage = !empty($item['PREVIEW_PICTURE_SECOND']) ? $item['PREVIEW_PICTURE_SECOND']['SRC'] : $item['PREVIEW_PICTURE']['SRC'];
-			?>
-			<span class="product-item-image-alternative" id="<?=$itemIds['SECOND_PICT']?>"
-				style="background-image: url('<?=$bgImage?>'); <?=($showSlider ? 'display: none;' : '')?>">
-			</span>
-			<?
-		}
-
-		if ($item['LABEL'])
-		{
-			?>
-			<div class="product-item-label-text <?=$labelPositionClass?>" id="<?=$itemIds['STICKER_ID']?>">
-				<?
-				if (!empty($item['LABEL_ARRAY_VALUE']))
-				{
-					foreach ($item['LABEL_ARRAY_VALUE'] as $code => $value)
-					{
-						?>
-						<div<?=(!isset($item['LABEL_PROP_MOBILE'][$code]) ? ' class="hidden-xs"' : '')?>>
-							<span title="<?=$value?>"><?=$value?></span>
-						</div>
-						<?
-					}
-				}
-				?>
-			</div>
-			<?
-		}
+	<span class="product-item-image-slider-slide-container slide" id="<?=$itemIds['PICT_SLIDER']?>">
+		<!-- для слайдера, но если уберу - НЕ БУДЕТ АНИМАЦИИ ПРИ НАВЕДЕНИИ (контент убрал) -->
+	</span>
+	<span class="product-item-image-original" id="<?=$itemIds['PICT']?>"
+		style="background-image: url('<?=$item['PREVIEW_PICTURE']['SRC']?>'); <?=($showSlider ? 'display: none;' : '')?>">
+	</span>
+	
+	<?
+	// БЕЗ ЭТОГО НЕ ОТОБРАЖАЕТСЯ ИНФОРМАЦИЯ ПРИ НАВЕДЕНИИ (ДОП ПОЛЯ КОЛИЧЕСТВА И КНОПКИ)
+	if ($item['SECOND_PICT'])
+	{
+		$bgImage = !empty($item['PREVIEW_PICTURE_SECOND']) ? $item['PREVIEW_PICTURE_SECOND']['SRC'] : $item['PREVIEW_PICTURE']['SRC'];
 		?>
+		<span class="product-item-image-alternative" id="<?=$itemIds['SECOND_PICT']?>"
+			style="background-image: url('<?=$bgImage?>'); <?=($showSlider ? 'display: none;' : '')?>">
+		</span>
+		<?
+	}
+
+	if ($item['LABEL'])
+	{
+		?>
+		<div class="product-item-label-text <?=$labelPositionClass?>" id="<?=$itemIds['STICKER_ID']?>">
+			<?
+			if (!empty($item['LABEL_ARRAY_VALUE']))
+			{
+				foreach ($item['LABEL_ARRAY_VALUE'] as $code => $value)
+				{
+					?>
+					<div<?=(!isset($item['LABEL_PROP_MOBILE'][$code]) ? ' class="hidden-xs"' : '')?>>
+						<span title="<?=$value?>"><?=$value?></span>
+					</div>
+					<?
+				}
+			}
+			?>
+		</div>
+		<?
+	}
+	?>
 
 	<!-- ЕСЛИ УБРАТЬ ПРОПАДАЮТ БЛОКИ ПРИ НАВЕДЕНИИ НА ОБЪЕКТ ВЫШЕ -->
 	<? if ($itemHasDetailUrl): ?>
@@ -89,6 +89,7 @@ use Bitrix\Main\Localization\Loc;
 	<? else: ?>
 	</span>
 	<? endif; ?>
+
 	<div class="product-item-title">
 		<? if ($itemHasDetailUrl): ?>
 		<a href="<?=$item['DETAIL_PAGE_URL']?>" title="<?=$productTitle?>">
@@ -99,107 +100,82 @@ use Bitrix\Main\Localization\Loc;
 		<? endif; ?>
 	</div>
 	
-	<?
-	if (!empty($arParams['PRODUCT_BLOCKS_ORDER']))
-	{
-		foreach ($arParams['PRODUCT_BLOCKS_ORDER'] as $blockName)
-		{
-			switch ($blockName)
+
+	<div class="product-item-price-container" data-entity="price-block">
+		<span class="product-item-price-old" id="<?=$itemIds['PRICE_OLD']?>" style="display: none;"></span>
+
+		<span class="product-item-price-current" id="<?=$itemIds['PRICE']?>">
+			<?= $item['ITEM_PRICES'][0]['PRINT_PRICE'] ?>
+		</span>
+	</div>     
+
+	<div class="product-item-info-container product-item-hidden" data-entity="quantity-block">
+		<div class="product-item-amount">
+			<div class="product-item-amount-field-container">
+				<span class="product-item-amount-field-btn-minus no-select" id="<?=$itemIds['QUANTITY_DOWN']?>"></span>
+				
+				<input class="product-item-amount-field" id="<?=$itemIds['QUANTITY']?>" type="number"
+					name="<?=$arParams['PRODUCT_QUANTITY_VARIABLE']?>"
+					value="<?= $measureRatio ?>">
+					
+				<span class="product-item-amount-field-btn-plus no-select" id="<?=$itemIds['QUANTITY_UP']?>"></span>
+				
+				<span class="product-item-amount-description-container">
+					<span id="<?=$itemIds['QUANTITY_MEASURE']?>"><?=$item['ITEM_MEASURE']['TITLE']?></span>
+					<span id="<?=$itemIds['PRICE_TOTAL']?>"></span>
+				</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="product-item-info-container product-item-hidden" data-entity="buttons-block">
+		<?
+			if ($actualItem['CAN_BUY'])
 			{
-				case 'price': ?>
-					<div class="product-item-price-container" data-entity="price-block">
-    					<span class="product-item-price-old" id="<?=$itemIds['PRICE_OLD']?>" style="display: none;"></span>
-            
-            			<span class="product-item-price-current" id="<?=$itemIds['PRICE']?>">
-                			<?= $item['ITEM_PRICES'][0]['PRINT_PRICE'] ?>
-            			</span>
-        			</div>     
-					<?
-					break;
-
-				case 'quantity':?>
-
-					<div class="product-item-info-container product-item-hidden" data-entity="quantity-block">
-						<div class="product-item-amount">
-							<div class="product-item-amount-field-container">
-								<span class="product-item-amount-field-btn-minus no-select" id="<?=$itemIds['QUANTITY_DOWN']?>"></span>
-								
-								<input class="product-item-amount-field" id="<?=$itemIds['QUANTITY']?>" type="number"
-									name="<?=$arParams['PRODUCT_QUANTITY_VARIABLE']?>"
-									value="<?= $measureRatio ?>">
-									
-								<span class="product-item-amount-field-btn-plus no-select" id="<?=$itemIds['QUANTITY_UP']?>"></span>
-								
-								<span class="product-item-amount-description-container">
-									<span id="<?=$itemIds['QUANTITY_MEASURE']?>"><?=$item['ITEM_MEASURE']['TITLE']?></span>
-									<span id="<?=$itemIds['PRICE_TOTAL']?>"></span>
-								</span>
-							</div>
-						</div>
-					</div>
-					<?
-
-					break;
-
-				case 'buttons':
-					?>
-					<div class="product-item-info-container product-item-hidden" data-entity="buttons-block">
-						<?
-							if ($actualItem['CAN_BUY'])
-							{
-								?>
-								<div class="product-item-button-container" id="<?=$itemIds['BASKET_ACTIONS']?>">
-									<a class="btn btn-default <?=$buttonSizeClass?>" id="<?=$itemIds['BUY_LINK']?>"
-										href="javascript:void(0)" rel="nofollow">
-										<?=($arParams['ADD_TO_BASKET_ACTION'] === 'BUY' ? $arParams['MESS_BTN_BUY'] : $arParams['MESS_BTN_ADD_TO_BASKET'])?>
-									</a>
-								</div>
-								<?
-							}
-							else
-							{
-								?>
-								<div>net knopka</div>
-								<?
-							}
-						?>
-					</div>
-					<?
-					break;
-
-				case 'props':
-
-						if ($arParams['ADD_PROPERTIES_TO_BASKET'] === 'Y' && !empty($item['PRODUCT_PROPERTIES']))
-						{
-							?>
-							<div id="<?=$itemIds['BASKET_PROP_DIV']?>" style="display: none;">
-								<?
-								if (!empty($item['PRODUCT_PROPERTIES_FILL']))
-								{
-									foreach ($item['PRODUCT_PROPERTIES_FILL'] as $propID => $propInfo)
-									{
-										?>
-										<input type="hidden" name="<?=$arParams['PRODUCT_PROPS_VARIABLE']?>[<?=$propID?>]"
-											value="<?=htmlspecialcharsbx($propInfo['ID'])?>">
-										<?
-										unset($item['PRODUCT_PROPERTIES'][$propID]);
-									}
-								}
-								?>
-							</div>
-							<?
-						}
-
-					break;
+				?>
+				<div class="product-item-button-container" id="<?=$itemIds['BASKET_ACTIONS']?>">
+					<a class="btn btn-default <?=$buttonSizeClass?>" id="<?=$itemIds['BUY_LINK']?>"
+						href="javascript:void(0)" rel="nofollow">
+						<?=($arParams['ADD_TO_BASKET_ACTION'] === 'BUY' ? $arParams['MESS_BTN_BUY'] : $arParams['MESS_BTN_ADD_TO_BASKET'])?>
+					</a>
+				</div>
+				<?
 			}
-		}
+			else
+			{
+				?>
+				<div>net knopka</div>
+				<?
+			}
+		?>
+	</div>
+
+	<?php
+	if ($arParams['ADD_PROPERTIES_TO_BASKET'] === 'Y' && !empty($item['PRODUCT_PROPERTIES']))
+	{
+		?>
+		<div id="<?=$itemIds['BASKET_PROP_DIV']?>" style="display: none;">
+			<?
+			if (!empty($item['PRODUCT_PROPERTIES_FILL']))
+			{
+				foreach ($item['PRODUCT_PROPERTIES_FILL'] as $propID => $propInfo)
+				{
+					?>
+					<input type="hidden" name="<?=$arParams['PRODUCT_PROPS_VARIABLE']?>[<?=$propID?>]"
+						value="<?=htmlspecialcharsbx($propInfo['ID'])?>">
+					<?
+					unset($item['PRODUCT_PROPERTIES'][$propID]);
+				}
+			}
+			?>
+		</div>
+		<?
 	}
 	?>
 </div>
 
-
 <?
-/*
+*/
 echo
 \TAO::frontend()->renderBlock(
 	'common/books-item',
@@ -208,7 +184,14 @@ echo
 		'itemIds' => $itemIds,
 		'photo' => $item['DETAIL_PICTURE']['SRC'],
 		'arParams' => $arParams,
+		'itemHasDetailUrl' => $itemHasDetailUrl,
+		'imgTitle' => $imgTitle,
+		'labelPositionClass' => $labelPositionClass,
+		'productTitle' => $productTitle,
+		'actualItem' => $actualItem,
+		'buttonSizeClass' => $buttonSizeClass,
+		'measureRatio' => $measureRatio,
+		'showSlider' => $showSlider,
 	]
 )
-	*/
 ?>
--- local/templates/galactic/frontend/src/block/common/books-item/books-item.blade.php
+++ local/templates/galactic/frontend/src/block/common/books-item/books-item.blade.php
@@ -1,33 +1,74 @@
-<div class="{{ $block }}" id="{{ $itemIds['ID'] }}" data-entity="item">
+<div class="product-item {{ $block }}">
 
-    <a
-    class="{{ $block->elem('link') }}" href="{{ $item['DETAIL_PAGE_URL'] }}" data-entity="image-wrapper">
-        <img 
-        class="{{ $block->elem('image') }}"
-        src="{{ $photo }}" 
-        alt="Book's image"
-        >
-    </a>
+    @if ($itemHasDetailUrl)
+        <a 
+        class="product-item-image-wrapper" 
+        href="{{ $item['DETAIL_PAGE_URL'] }}" 
+        title="{{ $imgTitle }}"
+		data-entity="image-wrapper">
+    @else
+        <span class="product-item-image-wrapper" data-entity="image-wrapper">
+    @endif
 
-    <div class="{{ $block->elem('info-container') }}">
+        <span class="product-item-image-slider-slide-container slide" id="{{ $itemIds['PICT_SLIDER'] }}">
+        </span>
+        <span class="product-item-image-original" id="{{ $itemIds['PICT'] }}"
+            style="background-image: url('{{ $item['PREVIEW_PICTURE']['SRC'] }}'); {!! ($showSlider ? 'display: none;' : '') !!}">
+        </span>
 
-        <a class="{{ $block->elem('title') }}" href="{{ $item['DETAIL_PAGE_URL'] }}">{!! $item['NAME'] !!}</a>
-        
-        <div class="{{ $block->elem('price-container') }}" data-entity="price-block">
-            <span class="product-item-price-old" id="{{ $itemIds['PRICE_OLD'] }}" style="display: none;"></span>
-            
-            <span class="product-item-price-current" id="{{ $itemIds['PRICE'] }}">
-                {!! $item['ITEM_PRICES'][0]['PRINT_PRICE'] !!}
+        @if ($item['SECOND_PICT'])
+            @php
+                $bgImage = !empty($item['PREVIEW_PICTURE_SECOND']) ? $item['PREVIEW_PICTURE_SECOND']['SRC'] : $item['PREVIEW_PICTURE']['SRC'];
+            @endphp
+            <span class="product-item-image-alternative" id="{{ $itemIds['SECOND_PICT'] }}"
+                style="background-image: url('{!! $bgImage !!}'); {!! ($showSlider ? 'display: none;' : '')!!}">
             </span>
-        </div>        
+        @endif
 
-        <div class="{{ $block->elem('amount-container') }}" data-entity="quantity-block">
+        @if ($item['LABEL'])
+            <div class="product-item-label-text {{ $labelPositionClass }}" id="{{ $itemIds['STICKER_ID'] }}">
+                @if (!empty($item['LABEL_ARRAY_VALUE']))
+                    @foreach ($item['LABEL_ARRAY_VALUE'] as $code => $value)
+                        <div{!! (!isset($item['LABEL_PROP_MOBILE'][$code]) ? ' class="hidden-xs"' : '')!!}>
+                            <span title="{{ $value }}">{{ $value }}</span>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        @endif
+
+	@if ($itemHasDetailUrl)
+	</a>
+	@else
+	</span>
+	@endif
+
+	<div class="product-item-title">
+		@if ($itemHasDetailUrl)
+		<a href="{{ $item['DETAIL_PAGE_URL'] }}" title="{{ $productTitle }}">
+		@endif
+		{!! $productTitle !!}
+		@if ($itemHasDetailUrl)
+		</a>
+		@endif
+	</div>
+
+    <div class="product-item-price-container" data-entity="price-block">
+        <span class="product-item-price-old" id="{{ $itemIds['PRICE_OLD'] }}" style="display: none;"></span>
+
+        <span class="product-item-price-current" id="{{ $itemIds['PRICE'] }}">
+            {!! $item['ITEM_PRICES'][0]['PRINT_PRICE'] !!}
+        </span>
+    </div>     
+
+    <div class="product-item-info-container product-item-hidden" data-entity="quantity-block">
+        <div class="product-item-amount">
             <div class="product-item-amount-field-container">
                 <span class="product-item-amount-field-btn-minus no-select" id="{{ $itemIds['QUANTITY_DOWN'] }}"></span>
                 
                 <input class="product-item-amount-field" id="{{ $itemIds['QUANTITY'] }}" type="number"
                     name="{{ $arParams['PRODUCT_QUANTITY_VARIABLE'] }}"
-                    value="{{ $item['ITEM_PRICES'][0]['MIN_QUANTITY'] }}">
+                    value="{!! $measureRatio !!}">
                     
                 <span class="product-item-amount-field-btn-plus no-select" id="{{ $itemIds['QUANTITY_UP'] }}"></span>
                 
@@ -37,38 +78,33 @@
                 </span>
             </div>
         </div>
+    </div>
 
-        <div class="{{ $block->elem('buttons-container') }}" data-entity="buttons-container">
-            <div id="{{ $itemIds['BASKET_ACTIONS'] }}" class="product-item-button-container">
-                <a class="{{ $block->elem('button-to-basket') }}" 
-                id="{{ $itemIds['BUY_LINK'] }}" 
-                href="javascript:void(0)" 
-                rel="nofollow">
-                    В корзину
+    <div class="product-item-info-container product-item-hidden" data-entity="buttons-block">
+        @if ($actualItem['CAN_BUY'])
+            <div class="product-item-button-container" id="{{ $itemIds['BASKET_ACTIONS'] }}">
+                <a class="btn btn-default {{ $buttonSizeClass }}" id="{{ $itemIds['BUY_LINK'] }}"
+                    href="javascript:void(0)" rel="nofollow">
+                    {!! ($arParams['ADD_TO_BASKET_ACTION'] === 'BUY' ? $arParams['MESS_BTN_BUY'] : $arParams['MESS_BTN_ADD_TO_BASKET']) !!}
                 </a>
             </div>
-        </div>
-
-        <div class="{{ $block->elem('tags-container') }}">
-
-            @if(!empty($item['LABEL_ARRAY_VALUE']['AUTHORS']))
-                <span class="{{ $block->elem('tag') }}">{{ $item['LABEL_ARRAY_VALUE']['AUTHORS'] }}</span>
-            @endif
-
-            @foreach ($item['PROPERTIES'] as $key => $property)
-
-                @if (!empty($property['VALUE'] && $key !== 'AUTHORS'))
-                    @if (is_array($property['VALUE']))
-                        <span class="{{ $block->elem('tag') }}">{!! implode(' / ', $property['VALUE']) !!}</span>
-                    @else
-                        <span class="{{ $block->elem('tag') }}">{{ $property['VALUE'] }}</span>
-                    @endif
-                @endif
-
-            @endforeach
-
-        </div>
-
+        @else
+            <div>net knopka</div>
+        @endif
     </div>
+
+@if ($arParams['ADD_PROPERTIES_TO_BASKET'] === 'Y' && !empty($item['PRODUCT_PROPERTIES']))
+	<div id="{{ $itemIds['BASKET_PROP_DIV'] }}" style="display: none;">
+		@if (!empty($item['PRODUCT_PROPERTIES_FILL']))
+			@foreach ($item['PRODUCT_PROPERTIES_FILL'] as $propID => $propInfo)
+				<input type="hidden" name="{{ $arParams['PRODUCT_PROPS_VARIABLE'] }}[{!! $propID !!}]"
+					value="{!! htmlspecialcharsbx($propInfo['ID']) !!}">
+				@php
+                    unset($item['PRODUCT_PROPERTIES'][$propID]);
+                @endphp
+			@endforeach
+		@endif
+	</div>
+@endif
 
 </div>
